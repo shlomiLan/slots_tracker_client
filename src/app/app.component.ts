bd9bf870f1125c21e0a1c70f6e127ddf7bdf3f64
shlomiLan <shlomilanton@gmail.com>
2018-10-06 09:11:23 +0300

    Remove not working code for disable back button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,10 +25,6 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
-
-      this.platform.registerBackButtonAction(() => {
-        //don't do anything
-      });
     });
   }
 }
