373de311cb524eb9daaa8b1777b86f9abf896c3b
shlomiLan <shlomilanton@gmail.com>
2019-02-14 08:50:20 +0200

    Update serer JS script

--- server.js
+++ server.js
@@ -19,7 +19,16 @@
 // });
 //
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});
+const express = require('express');
+const serveStatic = require("serve-static");
+const path = require('path');
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 3000;
+app.listen(port);
+
+//
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log(`Our app is running on port ${ PORT }`);
+// });
