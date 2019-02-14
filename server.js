bf132482faaec45b12eafefe0f6535046bbfac96
shlomiLan <shlomilanton@gmail.com>
2019-02-14 08:46:33 +0200

    Update serer JS script

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
-const http = require('http');
-const fs = require('fs');
-const httpPort = 5000;
+// const http = require('http');
+// const fs = require('fs');
+// const httpPort = 5000;
+//
+// http.createServer((req, res) => {
+//   fs.readFile('index.htm', 'utf-8', (err, content) => {
+//     if (err) {
+//       console.log('We cannot open "index.htm" file.')
+//     }
+//
+//     res.writeHead(200, {
+//       'Content-Type': 'text/html; charset=utf-8'
+//     });
+//
+//     res.end(content)
+//   })
+// }).listen(httpPort, "0.0.0.0", () => {
+//   console.log('Server listening on: http://localhost:%s', httpPort)
+// });
+//
 
-http.createServer((req, res) => {
-  fs.readFile('index.htm', 'utf-8', (err, content) => {
-    if (err) {
-      console.log('We cannot open "index.htm" file.')
-    }
-
-    res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8'
-    });
-
-    res.end(content)
-  })
-}).listen(httpPort, "0.0.0.0", () => {
-  console.log('Server listening on: http://localhost:%s', httpPort)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
