590805acaa42c348e616c99cc5f9e52a10376672
shlomiLan <shlomilanton@gmail.com>
2019-02-14 08:53:31 +0200

    Update serer JS script

--- server.js
+++ server.js
@@ -1,34 +1,34 @@
-// const http = require('http');
-// const fs = require('fs');
-// const httpPort = 5000;
-//
-// http.createServer((req, res) => {
-//   fs.readFile('index.htm', 'utf-8', (err, content) => {
-//     if (err) {
-//       console.log('We cannot open "index.htm" file.')
-//     }
-//
-//     res.writeHead(200, {
-//       'Content-Type': 'text/html; charset=utf-8'
-//     });
-//
-//     res.end(content)
-//   })
-// }).listen(httpPort, "0.0.0.0", () => {
-//   console.log('Server listening on: http://localhost:%s', httpPort)
-// });
-//
+const http = require('http');
+const fs = require('fs');
+const httpPort = process.env.PORT || 3000;
 
-const express = require('express');
-const serveStatic = require("serve-static");
-const path = require('path');
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 3000;
-app.listen(port);
+http.createServer((req, res) => {
+  fs.readFile('index.htm', 'utf-8', (err, content) => {
+    if (err) {
+      console.log('We cannot open "index.htm" file.')
+    }
 
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    });
+
+    res.end(content)
+  })
+}).listen(httpPort, () => {
+  console.log('Server listening on: http://localhost:%s', httpPort)
+});
+
+
+// const express = require('express');
+// const serveStatic = require("serve-static");
+// const path = require('path');
+// app = express();
+// app.use(serveStatic(path.join(__dirname, 'dist')));
+// const port = process.env.PORT || 3000;
+// app.listen(port);
 //
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//     console.log(`Our app is running on port ${ PORT }`);
-// });
+// //
+// // const PORT = process.env.PORT || 3000;
+// // app.listen(PORT, () => {
+// //     console.log(`Our app is running on port ${ PORT }`);
+// // });
