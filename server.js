9b9907f9b57016fe08d4ea2ca20018c7c335560c
shlomiLan <shlomilanton@gmail.com>
2019-02-16 12:03:11 +0200

    minor change

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 // server.js
 const express = require('express');
+const path = require('path');
 const serveStatic = require('serve-static');
 const history = require('connect-history-api-fallback');
 const port = process.env.PORT || 5000;
 
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
 app.use(history());
+app.use(express.static(__dirname));
+// app.use(serveStatic(__dirname + "/dist"));
 app.listen(port);
 
 
