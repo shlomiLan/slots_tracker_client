14527cf427bf1f1a3bb6918a5011ff95db90c617
shlomiLan <shlomilanton@gmail.com>
2019-02-16 08:26:02 +0200

    minor change

--- server.js
+++ server.js
@@ -1,32 +1,10 @@
-// // server.js
-// var express = require('express');
-// var path = require('path');
-// var serveStatic = require('serve-static');
-// app = express();
-// app.use(serveStatic(__dirname + "/dist"));
-// var port = process.env.PORT || 5000;
-// app.listen(port);
-// var history = require('connect-history-api-fallback');
-// app.use(history());
-
-
-const http = require('http');
-const fs = require('fs');
-const history = require('connect-history-api-fallback');
-const httpPort = process.env.PORT || 5000;
-
-http.createServer((req, res) => {
-  fs.readFile('index.html', 'utf-8', (err, content) => {
-    if (err) {
-      console.log('We cannot open "index.html" file.')
-    }
-
-    res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8'
-    });
-
-    res.end(content)
-  })
-}).listen(httpPort, () => {
-  console.log('Server listening on: http://localhost:%s', httpPort)
-}).use(history());
+// server.js
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
+app.listen(port);
+var history = require('connect-history-api-fallback');
+app.use(history());
