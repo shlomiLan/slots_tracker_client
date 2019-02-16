c8e4d02101d87e8f19210b697d1d04f9f6f2ba57
shlomiLan <shlomilanton@gmail.com>
2019-02-16 18:29:27 +0200

    Fix CORS

--- server.js
+++ server.js
@@ -7,5 +7,12 @@ app = express();
 app.use(history());
 app.use(serveStatic(__dirname + "/dist"));
 
+// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.listen(port);
 console.log('server started '+ port);
