204637dc1e352f749168941eec03fe8ca50d57ba
shlomiLan <shlomilanton@gmail.com>
2019-02-18 08:36:43 +0200

    Update test confs

--- config/test.env.js
+++ config/test.env.js
@@ -1,7 +1,7 @@
 const merge = require('webpack-merge');
-const devEnv = require('./dev.env');
+const prodEnv = require('./prod.env');
 
-module.exports = merge(devEnv, {
+module.exports = merge(prodEnv, {
   NODE_ENV: '"testing"',
   API_BASE_URL: '"https://slots-tracker-test.herokuapp.com"',
 });
