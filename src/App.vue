5fa16aa7cd1845d51c11e6da5f45909705100f1e
shlomiLan <shlomilanton@gmail.com>
2019-05-04 13:11:57 +0300

    Update notifications

--- src/App.vue
+++ src/App.vue
@@ -9,67 +9,12 @@
 </template>
 
 <script>
-import axios from 'axios';
 import drawer from './components/Drawer.vue';
-import firebase from '@/configFirebase.js';
-
-const { messaging } = firebase;
 
 export default {
   name: 'App',
   components: {
     drawer,
-  },
-  methods: {
-    getMessagingToken() {
-      console.log('*******');
-      console.log(messaging);
-      console.log('*******');
-      messaging.getToken().then(async (token) => {
-        if (token) {
-          const currentMessageToken = window.localStorage.getItem('messagingToken');
-          console.log('token will be updated', currentMessageToken !== token);
-          if (currentMessageToken !== token) {
-            await this.saveToken(token);
-          }
-        } else {
-          console.log('No Instance ID token available. Request permission to generate one.');
-          this.notificationsPermisionRequest();
-        }
-      });
-    },
-    notificationsPermisionRequest() {
-      messaging.requestPermission()
-        .then(() => {
-          this.getMessagingToken();
-        })
-        .catch((err) => {
-          console.log('Unable to get permission to notify.', err);
-        });
-    },
-    listenTokenRefresh() {
-      const currentMessageToken = window.localStorage.getItem('messagingToken');
-      console.log('currentMessageToken', currentMessageToken);
-      if (currentMessageToken) {
-        messaging.onTokenRefresh(() => {
-          messaging.getToken().then(function (token) {
-            this.saveToken(token);
-          }).catch((err) => {
-            console.log('Unable to retrieve refreshed token ', err);
-          });
-        });
-      }
-    },
-    saveToken(token) {
-      console.log('tokens', token);
-      axios.post('https://us-central1-slots-tracker-new.cloudfunctions.net/GeneralSubscription', { token })
-        .then((response) => {
-          window.localStorage.setItem('messagingToken', token);
-          console.log(response);
-        }).catch((err) => {
-          console.log(err);
-        });
-    },
   },
 };
 </script>
--- src/configFirebase.js
+++ src/configFirebase.js
@@ -3,25 +3,39 @@ import 'firebase/firestore';
 import 'firebase/messaging';
 import 'firebase/storage';
 
-const config = {
+// Your web app's Firebase configuration
+const firebaseConfig = {
   apiKey: 'AIzaSyAIBTEeDjBqsnu56eyTZUaXbX6PoNnTmL0',
   authDomain: 'slots-tracker-new.firebaseapp.com',
   databaseURL: 'https://slots-tracker-new.firebaseio.com',
   projectId: 'slots-tracker-new',
   storageBucket: 'slots-tracker-new.appspot.com',
   messagingSenderId: '424581987334',
+  appId: '1:424581987334:web:1e8f3c4166248069',
 };
 
-firebase.initializeApp(config);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-// Initialize Cloud Firestore through Firebase
 const db = firebase.firestore();
-
 db.enablePersistence({ experimentalTabSynchronization: true });
 
 const storage = firebase.storage();
 
 const messaging = firebase.messaging();
+messaging.usePublicVapidKey('BK4Odj0TLAUBNc7g3h9EZCpjexigiUdygZLAt5Drswe-cT8HwdEY_Sz5ICOsOzdwB3uQiicDbaU2h2yBz14js2M');
+
+// Request Permission of Notifications
+messaging.requestPermission().then(() => {
+  console.log('Notification permission granted.');
+
+  // Get Token
+  messaging.getToken().then((token) => {
+    console.log(token);
+  });
+}).catch((err) => {
+  console.log('Unable to get permission to notify.', err);
+});
 
 export default {
   db,
